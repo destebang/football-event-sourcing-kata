af10e0d77c12c96eb4fda476b65a7efe4d0aa33b
Daniel Esteban <danielestebangonzalez83@gmail.com>
2018-01-27 11:18:00 +0100

    Fix index.php and add symfony server

--- src/Gamescore/Domain/Service/Game/SimpleLineupPolicy.php
+++ src/Gamescore/Domain/Service/Game/SimpleLineupPolicy.php
@@ -2,17 +2,43 @@
 
 namespace Football\Gamescore\Domain\Service\Game;
 
+use Football\Gamescore\Domain\Aggregate\Player;
 use Football\Gamescore\Domain\Aggregate\Team;
 
 class SimpleLineupPolicy implements LineupPolicy
 {
     const STARTING_PLAYERS_NUMBER = 11;
-
     const MAXIMUM_BENCH_PLAYERS_NUMBER = 6;
 
     public function lineupIsValid(Team $team): bool
     {
-        return count($team->getOnGamePlayers()) === self::STARTING_PLAYERS_NUMBER
-            && count($team->getBenchPlayers()) <= self::MAXIMUM_BENCH_PLAYERS_NUMBER;
+        return $this->noPlayerIsRepeated($team)
+            && $this->gamePlayersNumberIsTheExpected($team->getOnGamePlayers())
+            && $this->benchPlayersNumberIsNotOverTheMaximum($team->getBenchPlayers());
+    }
+
+    private function noPlayerIsRepeated(Team $team): bool
+    {
+        $allPlayers = array_merge($team->getOnGamePlayers(), $team->getBenchPlayers());
+
+        return count($allPlayers) === count(array_unique($allPlayers));
+    }
+
+    /**
+     * @param array|Player[] $onGamePlayers
+     * @return bool
+     */
+    private function gamePlayersNumberIsTheExpected(array $onGamePlayers): bool
+    {
+        return count($onGamePlayers) === self::STARTING_PLAYERS_NUMBER;
+    }
+
+    /**
+     * @param array|Player[] $benchPlayers
+     * @return bool
+     */
+    private function benchPlayersNumberIsNotOverTheMaximum(array $benchPlayers): bool
+    {
+        return count($benchPlayers) <= self::MAXIMUM_BENCH_PLAYERS_NUMBER;
     }
 }
